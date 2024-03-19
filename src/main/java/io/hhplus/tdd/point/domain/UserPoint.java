877d52f4f1a2b37fb382a5a913bc1eeea7bbf84d
keepbang@kakao.com <rlqud1125@naver.com>
2024-03-19 23:37:51 +0900

    refactor: table 변경내용 적용

--- src/main/java/io/hhplus/tdd/point/domain/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/domain/UserPoint.java
@@ -1,10 +1,14 @@
 package io.hhplus.tdd.point.domain;
 
 public record UserPoint(
-    Long id,
-    Long point,
-    Long updateMillis
+    long id,
+    long point,
+    long updateMillis
 ) {
+
+  public static UserPoint empty(long id) {
+    return new UserPoint(id, 0, System.currentTimeMillis());
+  }
 
   public UserPoint increase(long amount) {
     amountValidation(amount);
